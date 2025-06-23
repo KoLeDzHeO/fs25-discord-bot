4b90339b6a3e58c4002969a8015a6bffaa136cc3
KoLeDzHeO <80348407+KoLeDzHeO@users.noreply.github.com>
2025-06-23 04:05:07 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import os
 import discord
 import asyncio
+import logging
+
+# Отключаем голосовой клиент, чтобы не вызывался audioop
+logging.getLogger("discord.voice_client").disabled = True
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+CHANNEL_ID = int(os.getenv("DISCORD_CHANNEL_ID"))
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f"Бот запущен как {client.user}")
-    channel_id = int(os.getenv("DISCORD_CHANNEL_ID"))
-    channel = client.get_channel(channel_id)
+    print(f"✅ Бот запущен как {client.user}")
+    channel = client.get_channel(CHANNEL_ID)
     while True:
         await channel.send("✅ Бот работает. (Тестовое сообщение)")
         await asyncio.sleep(300)  # каждые 5 минут
