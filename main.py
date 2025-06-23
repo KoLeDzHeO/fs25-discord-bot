d90f4ff62f9f07950e9378ddc09561a07aa1c4e0
KoLeDzHeO <80348407+KoLeDzHeO@users.noreply.github.com>
2025-06-23 04:33:57 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import discord
+import asyncio
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+CHANNEL_ID = int(os.getenv("DISCORD_CHANNEL_ID"))
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"✅ Бот запущен как {client.user}")
+    channel = client.get_channel(CHANNEL_ID)
+    while True:
+        await channel.send("✅ Бот работает. (Тестовое сообщение)")
+        await asyncio.sleep(300)
+
+client.run(TOKEN)
