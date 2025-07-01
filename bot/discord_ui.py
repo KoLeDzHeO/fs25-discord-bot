8d455da6d529c0669a7f82021b1d3385d522520a
KoLeDzHeO <80348407+KoLeDzHeO@users.noreply.github.com>
2025-07-01 16:15:56 +0200

    Delete bot directory

--- bot/discord_ui.py
+++
@@ -1,6 +0,0 @@
-import discord
-
-
-def create_report_embed(content: str) -> discord.Embed:
-    """Return a simple embed for vehicle reports."""
-    return discord.Embed(description=content, colour=discord.Colour.green())
