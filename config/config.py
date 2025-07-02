04c267fb886a16c4433bbeb0f0ef57b677c9fe17
KoLeDzHeO <80348407+KoLeDzHeO@users.noreply.github.com>
2025-07-02 21:07:44 +0200

    Update config.py

--- config/config.py
+++ config/config.py
@@ -11,7 +11,6 @@ class Config:
     api_base_url: str = os.getenv('API_BASE_URL')
     api_secret_code: str = os.getenv('API_SECRET_CODE')
     ftp_poll_interval: int = int(os.getenv('FTP_POLL_INTERVAL', 60))
-    message_id_file: str = os.getenv('MESSAGE_ID_FILE', 'message_id.json')
     ftp_host: str = os.getenv('FTP_HOST', '')
     ftp_port: int = int(os.getenv('FTP_PORT', 21))
     ftp_user: str = os.getenv('FTP_USER', '')
